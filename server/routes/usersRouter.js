373cc4f6f5b18a9e41ed5c908621147c66091bfe
Owen Jones <owenthefirst93@gmail.com>
2019-11-07 18:47:06 -0500

    Working on usersRouter

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -13,19 +13,71 @@ const router = express.Router();
 router.get('/', async (req, res) => {
     const inputQuery = ('SELECT * FROM users')
     const data = await db.any(inputQuery)
-    res.json({
-        message: 'Getting all /users',
-        payload: data
-    })
+    try{
+        res.json({
+            message: 'Getting all /users',
+            payload: data
+        })
+    } catch (error){
+        res.json({
+            message: 'Error not a valid input try another'
+        })
+    }
 })
 
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id
+router.get('/:id', async(req, res) => {
+    const id = parseInt(req.params.id)
+    const inputQuery = (`SELECT * FROM users WHERE id =${id}`)
+    const data = await db.one(inputQuery)
+    try{
+        res.json({
+            message: `Getting user ${id}`,
+            payload: data
+        })
+    } catch (error){
+        res.json({
+            message: 'Error try another input'
+        })
+    }
     
 })
 
-router.post('/')
-router.delete('/:id')
+router.post('/', async (req, res) => {
+    const user = req.body
+    const inputQuery = (`INSERT INTO users(username, password, firstname, lastname) VALUES($1, $2, $3, $4)`) 
+    
+    try{
+        await db.none(inputQuery,[user.username, user.password, user.firstname, user.lastname])
+        res.json({
+            message: "Success user added to database",
+            payload: req.body
+        })
+    } catch(error){
+        res.json({
+            message: "Could not add user. Something went wrong"
+        })
+        console.log(error)
+    }
+})
+
+router.delete('/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    const inputQuery = (`DELETE FROM users WHERE id=${id}`);
+    const deleteUser = await db.one(inputQuery)
+
+    try{
+        
+        res.json({
+            message: 'Success. User deleted.',
+            payload: deleteUser
+        })
+    } catch(error){
+        res.json({
+            message: 'Could not delete user. Try anoher userId'
+        })
+        console.log(error)
+    }
+})
 
 module.exports = router;
