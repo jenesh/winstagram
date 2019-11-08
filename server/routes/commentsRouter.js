edd3d6a02c709bffa2cbc1f938b22ff89a54ee85
Owen Jones <owenthefirst93@gmail.com>
2019-11-08 11:42:46 -0500

    usersfixes and started on comments

--- server/routes/commentsRouter.js
+++ server/routes/commentsRouter.js
@@ -4,3 +4,47 @@
 // POST /comments/posts/:post_id/:commenter_id - Add single comment.
 // PATCH /comments/:post_id/:commenter_id - Edit single comment.
 // DELETE /comments/:post_id/:commenter_id - Delete single comment.
+
+const express = require('express')
+const cors = require('cors')
+const db = require('../pgPromise')
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//     res.send('Comments home')
+// })
+
+router.get('/posts/:post_id', async(req, res) => {
+    const postId = parseInt(req.params.post_id)
+    const inputQuery = (`SELECT body FROM comments WHERE post_id = $1`)
+
+    try{
+        const getPosts = await db.any(inputQuery, [postId])
+        res.json({
+            message: 'Success. Get request received',
+            payload: getPosts,
+            success: true
+        })
+    }catch (error){
+        res.json({
+            message: "Error wrong input. Enter a different post id",
+            success: false
+        })
+    }
+})
+
+router.post('/posts/:post_id/:commenter_id', (req, res) => {
+    const postId = req.body.post_id
+    const commenterId = req.body.commenter_id
+    const inputQuery = (`SELECT `)
+})
+
+router.patch('/:post_id/:commenter_id', (req, res) => {
+
+})
+
+router.delete('/posts/:commenter_id', (req, res) => {
+
+})
+
+module.exports = router
