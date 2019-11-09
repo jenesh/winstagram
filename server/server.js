5b413b1b3592e08d1a718b2c99a2b841275e546e
Sharrar Khan <SharrarKhan728@gmail.com>
2019-11-09 10:21:23 -0500

    I did posts and likes routes

--- server/server.js
+++ server/server.js
@@ -1,19 +1,22 @@
-// Main server
 const express = require('express');
 const cors = require('cors');
 const db = require('./pgPromise');
 
-const app = express(); // Creating express server
+const app = express(); 
 
-app.use(cors()); // Allows cors interaction
-app.use(express.urlencoded({extended: false})); // Required to parse into useable js
-app.use(express.json()); // Required for parsing JSON
+app.use(cors()); 
+app.use(express.urlencoded({extended: false}));
+app.use(express.json()); 
 
-//Routes:
+
 const postsRoutes = require("./routes/postsRouter");
 app.use("/posts", postsRoutes);
 
-// TEST Route
+
+const likesRouter = require("./routes/likesRouter");
+app.use("/likes", likesRouter);
+
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Server is up and running',
@@ -21,17 +24,14 @@ app.get('/', (req, res) => {
     })
 });
 
-// USERS Route
+
 const usersRouter = require('./routes/usersRouter.js')
 app.use('/users', usersRouter)
 
-
-
-// ALBUMS Route
 const albumsRouter = require('./routes/albumsRouter');
 app.use('/albums', albumsRouter);
 
-// PICTURES Route
+
 const picturesRouter = require('./routes/picturesRouter');
 app.use('/pictures', picturesRouter);
 
